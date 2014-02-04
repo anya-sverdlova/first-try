d03216cc2e3bf9e3cd7928a22b3fe269ea62a854
Anna <annarchy@list.ru>
2014-02-04 11:01:12 +0200

    all

---
+++ js/lib/ann.js
@@ -0,0 +1,11 @@
+function getId(id) {
+	return document.getElementById(id);
+	}
+
+function getClass(clasName) {
+	return document.getElementsByClassName(clasName);
+	}
+	
+function getElts(elt) {
+	return document.getElementsByTagName(elt);
+	}
