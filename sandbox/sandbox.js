a8a1b2b91368dafe8d5e109c88320501c8510104
Anna <annarchy@list.ru>
2014-02-25 18:45:03 +0200

    +position correction

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,27 +1,26 @@
 ﻿
 function startSandBox(){
 
-    /*var byTag = ann('span');
-    console.log(byTag);
+	var byTag = ann('span');
+	console.log(byTag);
 
 	var byClass = ann('.class-inner');
-    //console.log(byClass.getDomElements());
 	console.log(byClass);
 	
-    var byClasses = ann('.class1.class3');
-    console.log(byClasses);
-	//byClasses.domElements[0].innerHTML = 'rrr';
+	var byClasses = ann('.class1.class3');
+	console.log(byClasses);
+	byClasses.domElements[0].innerHTML = 'rrr';
 	
 	var byId = ann('#someID');
-    console.log(byId); */
+	console.log(byId); 
 	
 	var byTagPlusClass = ann('span.class1');
 	console.log(byTagPlusClass);
 	byTagPlusClass.domElements[0].innerHTML = 'red';
 	byTagPlusClass.domElements[1].innerHTML = 'not red';
 	
-	//var byTagPlusClass2 = ann('span.class1.class3');
-	//console.log(byTagPlusClass2);
+	var byTagPlusClass2 = ann('span.class1.class3');
+	console.log(byTagPlusClass2);
 	
 	//var answer = ann.annObject.getRequest('index2.html');
 	//ann.httpRequest(requestParams, callback, errorCallback);
